1c992704dd0befd159bd0f1a9dbf28a0191fefa9
Keefe Tan Kui Yu <keefe@Keefes-MacBook-Pro.local>
2022-04-05 11:04:49 +0800

    initial commit

---
+++ src/test/java/vttp2022/paf/day12giphy/Day12GiphyApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.paf.day12giphy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day12GiphyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
